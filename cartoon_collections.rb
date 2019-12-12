d0c348b9ba36f868b10dc752ddd004c36561d42a
Paul Hoffart <pahoffart25@gmail.com>
2019-12-12 20:08:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,11 @@ def summon_captain_planet(array)
   array.map { |e| e.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  if array.map { |e| e.length < 4}
+    return true
+  else
+    return false
 end
 
 def find_the_cheese# code an argument here
