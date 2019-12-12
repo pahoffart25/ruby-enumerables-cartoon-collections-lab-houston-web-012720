f362e8b3ec0bbcd21d795be0ed79d9ac7e05a7cf
Paul Hoffart <pahoffart25@gmail.com>
2019-12-12 20:42:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,9 +17,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include? ("cheddar" "gouda" "camembert")
-    return "cheddar"
-  else
-    return nil
+    array.find do |type|
+    cheese_types.include?(type)
   end
 end
