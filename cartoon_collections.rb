534a5c2f66111d571a49cc0dcb9549b01facd6d4
Paul Hoffart <pahoffart25@gmail.com>
2019-12-12 20:12:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.map { |e| e === cheese_types }
+  if array.map { |e| "#{e}" === cheese_types }
     return "#{e}"
   else
     return nil
